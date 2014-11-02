772eda8eb9b1b4942e080a6af3e1b7dfa7eb2391
peckjon <peckjon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-02 05:16:13 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/clean-protected-and-private-page-titles/trunk@1018179 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cleantitles.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Plugin Name: Clean Protected and Private Page Titles
+ * Plugin URI: http://seemeroar.com/plugins/cleantitles
+ * Description: Some themes prefix page titles with 'Protected' or 'Private'; this plugin removes those words from titles
+ * Version: 0.1
+ * Author: Jon Peck
+ * Author URI: http://seemeroar.com
+ * License: GPL v2 - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+/*  Copyright 2014  Jon Peck  (email : plugins@seemeroar.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+function the_title_clean($title) {
+	return preg_replace(array('#Protected:#','#Private:#'),array('',''), attribute_escape($title));
+}
+add_filter('the_title', 'the_title_clean');
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== Plugin Name ===
+Contributors: peckjon
+Donate link: http://seemeroar.com/donate/
+Tags: comments, spam
+Requires at least: 3.0.1
+Tested up to: 4.0
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Some themes prefix post titles with "Protected" or "Private"; this plugin removes those prefixes
+
+== Description ==
+
+Wordpress allows editors to mark Posts as "Protected" or "Private".
+
+In some Themes, the words "Protected:" or "Private:" are automatically placed before the title of each post.
+
+This plugin removes those words from the titles.
+
+== Installation ==
+
+1. Upload the "cleantitle" folder and its contents to the "/wp-content/plugins/" directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+N/A
+
+== Screenshots ==
+
+N/A
+
+== Changelog ==
+
+= 0.1 =
+* initial version of the plugin
+
+== Upgrade Notice ==
+
+N/A
